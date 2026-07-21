9a936225fde102b1879c225631fe01c97b4c1203
Bintang Mada <44053984+bintangmada@users.noreply.github.com>
2026-07-21 07:09:24 +0700

    feat: add props to a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-const user = {
-  firstName : "Bintang",
-  lastName : "Mada",
-  age : 21
-}
+import Greeting from "./components/Greeting"
 
-function greeting(){
-  return "Hallo react";
-}
-
-
-function App() {
-  return (
-    <>
-      <h1>Hello {user.firstName} {user.lastName}</h1>
-      <h1>Age: {user.age}</h1>
-      <h1>Next year: {user.age+1}</h1>
-      <p>{greeting()}</p>
-    </>
+function App(){
+  return(
+    <Greeting name = "Bintang" age = {21}/>
   )
 }
 
